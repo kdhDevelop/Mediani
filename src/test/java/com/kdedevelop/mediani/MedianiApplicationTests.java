ebb6ce8ff0de83f31302edf06d4845c8fb366215
KDH <kde6571@gmail.com>
2025-10-13 15:06:43 +0900

    Init: Project Init

---
+++ src/test/java/com/kdedevelop/mediani/MedianiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdedevelop.mediani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedianiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
